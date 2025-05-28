1172f8dbeebc586855c06ddec657877c5cdc122f
aflamiano <aflamiano.career@gmail.com>
2025-05-29 05:02:12 +0800

    Config update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "four-card-feature-section",
+  base: "/four-card-feature-section",
 });
